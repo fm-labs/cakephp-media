b47ae5c2c0a2b4055883adfe98b1156be65fbbcd
fm-labs <flowmotionlabs@gmail.com>
2016-01-17 20:51:10 +0100

    removed auto config loading from bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,4 +1,11 @@
 <?php
-Cake\Core\Configure::load('media');
 
-\Cake\Database\Type::map('media_file', 'Media\Database\Type\MediaFileType');
+use Cake\Core\Configure;
+use Cake\Database\Type;
+
+if (!Configure::read('Media')) {
+    die("Media Plugin not configured");
+}
+
+
+Type::map('media_file', 'Media\Database\Type\MediaFileType');
