2ede7b8a0a210393c94ddc2aaf909bc3601b582d
fm-labs <flowmotionlabs@gmail.com>
2016-02-17 04:45:22 +0100

    changed default admin media route scope to /mediaadmin

--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,24 @@
 <?php
 use Cake\Routing\Router;
 
-Router::plugin('Media', function ($routes) {
-    $routes->prefix('admin', function ($routes) {
+// Media admin routes
+Router::scope(
+    '/mediaadmin',
+    ['plugin' => 'Media', 'prefix' => 'admin', '_namePrefix' => 'media:'],
+    function ($routes) {
 
         $routes->extensions(['json']);
 
-        $routes->connect('/browser/', ['plugin' => 'Media', 'controller' => 'MediaBrowser', 'action' => 'tree', 'config' => 'default']);
-        $routes->connect('/browser/:config/', ['plugin' => 'Media', 'controller' => 'MediaBrowser', 'action' => 'tree']);
-        $routes->connect('/browser/:config/:action', ['plugin' => 'Media', 'controller' => 'MediaBrowser']);
+        $routes->connect('/browser/',
+            ['plugin' => 'Media', 'controller' => 'MediaBrowser', 'action' => 'tree', 'config' => 'default']
+        );
+        $routes->connect('/browser/:config/',
+            ['plugin' => 'Media', 'controller' => 'MediaBrowser', 'action' => 'tree']
+        );
+        $routes->connect('/browser/:config/:action',
+            ['plugin' => 'Media', 'controller' => 'MediaBrowser']
+        );
 
         $routes->connect('/:controller');
         $routes->fallbacks('DashedRoute');
-    });
 });
