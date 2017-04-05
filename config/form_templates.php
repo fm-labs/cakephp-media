3ae9d26c09fe7d96b772d93cba59cc0f9d9bbe56
fm-labs <flowmotionlabs@gmail.com>
2017-04-05 23:28:47 +0200

    media picker string templates

---
+++ config/form_templates.php
@@ -0,0 +1,8 @@
+<?php
+return [
+
+    // Media picker widget templates
+    'media_mediapicker' => '<div{{attrs}}>{{input}}{{actions}}</div><script>{{script}}</script>',
+    'media_mediapicker_actions' => '<div class="media-picker-actions">{{select}}&nbsp;{{remove}}</div>',
+
+];
