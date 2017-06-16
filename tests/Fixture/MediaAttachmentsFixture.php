e7e25f852e7d3aabb9c0b17a3bbb2dbf45a8e607
fm-labs <flowmotionlabs@gmail.com>
2017-06-16 21:23:01 +0200

    added fixtures

---
+++ tests/Fixture/MediaAttachmentsFixture.php
@@ -0,0 +1,26 @@
+<?php
+namespace Media\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * MediaAttachmentsFixture
+ *
+ */
+class MediaAttachmentsFixture extends TestFixture
+{
+
+    /**
+     * Import
+     *
+     * @var array
+     */
+    public $import = ['table' => 'attachments'];
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [];
+}
